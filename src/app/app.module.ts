893ad111242da90a84c36497d69c0ddf1af8bf08
Geb54g <gabriel.ndolo@student.moringaschool.com>
2022-04-28 23:38:17 +0300

    ft:generated directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FontDirective } from './font.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FontDirective
   ],
   imports: [
     BrowserModule,
