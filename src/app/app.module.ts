5de3dffaf948a0ef54783419e0f99c17e4a7fe83
Geb54g <gabriel.ndolo@student.moringaschool.com>
2022-04-28 23:52:15 +0300

    ft:generated pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontDirective } from './font.directive';
+import { NotFoundComponent } from './not-found/not-found/not-found.component';
+import { LastPipe } from './pipe/last.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FontDirective
+    FontDirective,
+    NotFoundComponent,
+    LastPipe
   ],
   imports: [
     BrowserModule,
