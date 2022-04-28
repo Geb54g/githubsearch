a226a5504f01610853673fc069a40d3a471d5cfc
Geb54g <gabriel.ndolo@student.moringaschool.com>
2022-04-29 00:05:52 +0300

    ft:generated a service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
-
+import { CommonModule } from '@angular/common';
+import {RouterModule,Routes} from '@angular/router'
+import { AppComponent } from '../app.component';
+import { UsersComponent } from '../users/users.component';
+import { ReposComponent } from '../repos/repos.component';
+import { NotFoundComponent } from '../not-found/not-found.component';
+// Defining routes
+const routes:Routes=[
+  {path:"users",component:UsersComponent},
+  {path:"repos",component:ReposComponent},
+  {path:"",redirectTo:"/users",pathMatch:"full"},
+  {path:'**',component:NotFoundComponent}
+]
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports:[RouterModule],
+  declarations: []
 })
-export class AppRoutingModule { }
+export class RoutingModule { }
+
+
+
+
+
+
+
+
+
